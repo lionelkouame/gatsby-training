c8f2c1979ba088d04c16966c3af68099d66dffc8
Lionel Kouame <lionelkouame@gmail.com>
2020-04-11 02:31:24 +0200

    #2 - add menu and layout

--- hello-world/gatsby-config.js
+++ hello-world/gatsby-config.js
@@ -3,7 +3,13 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
 module.exports = {
-  /* Your site config here */
-}
+    plugins: [
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography`,
+            },
+        },
+    ],
+};
